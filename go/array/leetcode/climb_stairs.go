dcd6fa396e07416ed6437f5f0ddfe35aae435bad
mengmeng.duan 段猛猛 <mengmeng.duan@qiyuesuo.com>
2020-09-07 23:22:07 +0800

    go:数组leetcode题目，移动零和爬楼梯问题

---
+++ go/array/leetcode/climb_stairs.go
@@ -0,0 +1,40 @@
+package main
+
+//递归，指数级时间复杂度
+//运行结果：超时
+func climbStairs1(n int) int {
+	if n <= 2 {
+		return n
+	}
+
+	return climbStairs1(n-1) + climbStairs1(n-2)
+}
+
+//备忘录
+var mem = map[int]int{}
+
+func climbStairs2(n int) int {
+	if n <= 2 {
+		return n
+	}
+
+	if _, exist := mem[n]; exist {
+		return mem[n]
+	}
+	res := climbStairs2(n-1) + climbStairs2(n-2)
+	mem[n] = res
+	return res
+}
+
+//动态规划的思想
+func climbStairs3(n int) int {
+	if n <= 2 {
+		return n
+	}
+
+	prev, next := 1, 2
+	for i := 2; i < n; i++ {
+		prev, next = next, prev+next
+	}
+	return next
+}
