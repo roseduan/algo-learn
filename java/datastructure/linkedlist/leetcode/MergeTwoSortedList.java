1421e1b6c22f7cafdc1a7fad943dea5b66f75dca
mengmeng.duan 段猛猛 <mengmeng.duan@qiyuesuo.com>
2020-10-20 11:09:04 +0800

    java:链表题目

--- java/datastructure/linkedlist/leetcode/MergeTwoSortedList.java
+++ java/datastructure/linkedlist/leetcode/MergeTwoSortedList.java
@@ -7,7 +7,30 @@ package datastructure.linkedlist.leetcode;
  */
 public class MergeTwoSortedList {
 
+    /**
+     * 递归
+     */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    /**
+     * 迭代的写法
+     */
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
         ListNode node = new ListNode(-1);
         ListNode p = node;
         while (l1 != null && l2 != null) {
